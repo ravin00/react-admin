f6631c6bc8bf3bd0161a97428a9b131bdac584bb
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2024-06-17 16:27:47 +0530

    dark mode light mode function finished !!!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { ColorModeContext, useMode } from "./theme"; 
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from './scenes/global/Topbar';
+
 
 function App() {
-  return (
-    <div className="App">
+
+  const [theme,colorMode] = useMode();
+
+  return (<ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+  <div className="App">
+    <main className="content">
+      <Topbar />
+    </main>
     </div>
+  </ThemeProvider>
+  </ColorModeContext.Provider>
+  
   );
 }
 
